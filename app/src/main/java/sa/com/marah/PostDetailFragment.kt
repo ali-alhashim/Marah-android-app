df6f7c3f4f7c3f2cf5e244822597062a667f3fbd
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2023-12-02 20:31:26 +0300

    post detail with photos

--- app/src/main/java/sa/com/marah/PostDetailFragment.kt
+++ app/src/main/java/sa/com/marah/PostDetailFragment.kt
@@ -9,8 +9,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -28,6 +33,8 @@ class PostDetailFragment(postId: Int) : Fragment() {
     private lateinit var createdBy: TextView
     private lateinit var postText:TextView
     private lateinit var postSubject:TextView
+    private lateinit var postPath:TextView
+    private lateinit var postLayout:LinearLayout
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,9 +43,11 @@ class PostDetailFragment(postId: Int) : Fragment() {
         val view = inflater.inflate(R.layout.fragment_post_detail, container, false)
         Log.i(TAG, "this Fragment for post detail Id = ${postId}")
 
-        createdBy = view.findViewById(R.id.d_post_createdby)
-        postText  = view.findViewById(R.id.d_post_text)
+        createdBy   = view.findViewById(R.id.d_post_createdby)
+        postText    = view.findViewById(R.id.d_post_text)
         postSubject = view.findViewById(R.id.d_post_subject)
+        postPath    = view.findViewById(R.id.d_post_path)
+        postLayout  = view.findViewById(R.id.d_post_layout)
 
         loadPostDetail(postId)
         return view
@@ -73,6 +82,30 @@ class PostDetailFragment(postId: Int) : Fragment() {
                         postSubject.text = post.post_subject
                         createdBy.text = post.username
                         postText.text = post.post_text
+                        postPath.text = "${post.id} / ${post.post_category} / ${post.post_subcategory}"
+                        val requestOptions = RequestOptions().centerCrop()
+                        for(image in post.post_images)
+                        {
+                            val imageView = ImageView(requireContext())
+
+                            Glide.with(requireContext())
+                                .load("${MainActivity().BASE_URL}${image.removePrefix("/")}")
+                                .apply(requestOptions)
+                                .transition(DrawableTransitionOptions.withCrossFade())
+                                .into(imageView)
+
+
+                            val layoutParams =  LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+
+                            layoutParams.setMargins(0, 1,0, 1)
+                            imageView.setPadding(1,1,1,1)
+                            imageView.scaleType = ImageView.ScaleType.CENTER_INSIDE
+                            imageView.layoutParams = layoutParams
+
+
+
+                            postLayout.addView(imageView)
+                        }
 
 
 
