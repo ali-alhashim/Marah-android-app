a332d28681708dc211baa5b72cca37845c3ab549
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2023-12-26 20:56:14 +0300

    MessageRecyclerAdapter

---
+++ app/src/main/java/sa/com/marah/Data/MessageRecyclerAdapter.kt
@@ -0,0 +1,4 @@
+package sa.com.marah.Data
+
+class MessageRecyclerAdapter {
+}
--- app/src/main/java/sa/com/marah/MessagesFragment.kt
+++ app/src/main/java/sa/com/marah/MessagesFragment.kt
@@ -5,17 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
 
 class MessagesFragment : Fragment() {
-
+    private lateinit var MessagesRecyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_messages, container, false)
+        val view =  inflater.inflate(R.layout.fragment_messages, container, false)
+        MessagesRecyclerView = view.findViewById(R.id.MessagesRecyclerView)
+
+        loadMessagesList()
+        return view
+    }
+
+    fun loadMessagesList()
+    {
+
     }
 
 
